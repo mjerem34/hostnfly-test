12e7cab6e398645a463d0a32cf44d0f14cf66f63
Adrien Viviani <adrien.viv@gmail.com>
2022-09-07 17:09:07 +0200

    refacto/improve reservation and booking model tests

--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -8,9 +8,7 @@ describe Booking, type: :model do
 
   describe 'is after start' do
     context 'when the start_date is before the end_date' do
-      let(:booking) do
-        build(:booking, start_date: '2021-01-01', end_date: '2021-01-10')
-      end
+      let(:booking) { build(:booking, start_date: '2021-01-01', end_date: '2021-01-10') }
 
       it 'is valid' do
         booking.validate
@@ -19,16 +17,12 @@ describe Booking, type: :model do
     end
 
     context 'when the start_date is after the end_date' do
-      let(:booking) do
-        build(:booking, start_date: '2021-01-10', end_date: '2021-01-01')
-      end
+      let(:booking) { build(:booking, start_date: '2021-01-10', end_date: '2021-01-01') }
 
       it 'is not valid' do
-        I18n.with_locale(:en) do
-          booking.validate
-
-          expect(booking.errors.details).to eq({ start_date: [{ error: :greater_than_end_date }] })
-        end
+        booking.validate
+        expect(booking.errors.details)
+          .to eq({ start_date: [{ error: :greater_than_end_date }] })
       end
     end
   end
@@ -36,24 +30,35 @@ describe Booking, type: :model do
   context 'when there is already a booking on these dates' do
     let!(:listing) { create(:listing) }
     let(:booking_2) do
-      build(:booking, start_date: 2.days.from_now, end_date: 3.days.from_now,
-                      listing: listing)
+      build(:booking, start_date: 3.days.from_now, end_date: 4.days.from_now, listing: listing)
     end
 
     before do
-      create(:booking, start_date: 2.days.from_now, end_date: 3.days.from_now,
-                       listing: listing)
+      create(:booking, start_date: 2.days.from_now, end_date: 7.days.from_now, listing: listing)
     end
 
     it 'adds errors for overlapping other booking' do
-      I18n.with_locale(:en) do
-        booking_2.validate
+      booking_2.validate
+      expect(booking_2.errors.details)
+        .to eq({ start_date: [{ error: :overlap }], end_date: [{ error: :overlap }] })
+    end
+  end
 
-        expect(booking_2.errors.details)
-          .to eq({ listing_id: [{ error: :taken, value: 1 }],
-                   start_date: [{ error: :overlap }],
-                   end_date: [{ error: :overlap }] })
-      end
+  context 'when an other booking has the same start_date and last_date on the listing' do
+    let!(:listing) { create(:listing) }
+    let(:booking_2) do
+      build(:booking, start_date: 2.days.from_now, end_date: 4.days.from_now, listing: listing)
+    end
+
+    before do
+      create(:booking, start_date: 2.days.from_now, end_date: 4.days.from_now, listing: listing)
+    end
+
+    it 'adds errors uniquess of listing index' do
+      booking_2.validate
+      expect(booking_2.errors.details)
+        .to eq({ listing_id: [{ error: :taken, value: 1 }],
+                 start_date: [{ error: :overlap }], end_date: [{ error: :overlap }] })
     end
   end
 end
--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -8,9 +8,7 @@ describe Reservation, type: :model do
 
   describe 'is after start' do
     context 'when the start_date is before the end_date' do
-      let(:reservation) do
-        build(:reservation, start_date: '2021-01-01', end_date: '2021-01-10')
-      end
+      let(:reservation) { build(:reservation, start_date: '2021-01-01', end_date: '2021-01-10') }
 
       it 'is valid' do
         reservation.validate
@@ -19,17 +17,12 @@ describe Reservation, type: :model do
     end
 
     context 'when the start_date is after the end_date' do
-      let(:reservation) do
-        build(:reservation, start_date: '2021-01-10', end_date: '2021-01-01')
-      end
+      let(:reservation) { build(:reservation, start_date: '2021-01-10', end_date: '2021-01-01') }
 
       it 'is not valid' do
-        I18n.with_locale(:en) do
-          reservation.validate
-
-          expect(reservation.errors.details)
-            .to eq({ start_date: [{ error: :greater_than_end_date }] })
-        end
+        reservation.validate
+        expect(reservation.errors.details)
+          .to eq({ start_date: [{ error: :greater_than_end_date }] })
       end
     end
   end
@@ -37,24 +30,35 @@ describe Reservation, type: :model do
   context 'when there is already a reservation on these dates' do
     let!(:listing) { create(:listing) }
     let(:reservation_2) do
-      build(:reservation, start_date: 2.days.from_now, end_date: 3.days.from_now,
-                          listing: listing)
+      build(:reservation, start_date: 3.days.from_now, end_date: 4.days.from_now, listing: listing)
     end
 
     before do
-      create(:reservation, start_date: 2.days.from_now, end_date: 3.days.from_now,
-                           listing: listing)
+      create(:reservation, start_date: 2.days.from_now, end_date: 7.days.from_now, listing: listing)
     end
 
     it 'adds errors for overlapping other reservation' do
-      I18n.with_locale(:en) do
-        reservation_2.validate
+      reservation_2.validate
+      expect(reservation_2.errors.details)
+        .to eq({ start_date: [{ error: :overlap }], end_date: [{ error: :overlap }] })
+    end
+  end
 
-        expect(reservation_2.errors.details)
-          .to eq({ listing_id: [{ error: :taken, value: 1 }],
-                   start_date: [{ error: :overlap }],
-                   end_date: [{ error: :overlap }] })
-      end
+  context 'when an other reservation has the same start_date and last_date on the listing' do
+    let!(:listing) { create(:listing) }
+    let(:reservation_2) do
+      build(:reservation, start_date: 2.days.from_now, end_date: 4.days.from_now, listing: listing)
+    end
+
+    before do
+      create(:reservation, start_date: 2.days.from_now, end_date: 4.days.from_now, listing: listing)
+    end
+
+    it 'adds errors uniquess of listing index' do
+      reservation_2.validate
+      expect(reservation_2.errors.details)
+        .to eq({ listing_id: [{ error: :taken, value: 1 }],
+                 start_date: [{ error: :overlap }], end_date: [{ error: :overlap }] })
     end
   end
 end
